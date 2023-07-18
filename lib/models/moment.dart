eb23e6241dea040a424d66be5bd23e5973e616ea
Luxe7 <luxe7@foxmail.com>
2023-07-18 22:40:06 +0800

    添加使用说明

--- lib/models/moment.dart
+++ lib/models/moment.dart
@@ -76,132 +76,141 @@ class Moment {
   static List<Moment> mocks() {
     return <Moment>[
       Moment(
+        id: '1',
+        content: '''我爱上这尘世纷纷扰扰的相遇\n不停重复俗气又沉重的春天\n爱上这承受的一切，又粉碎的决心''',
+        user: User(
           id: '1',
-          content: '今天天气真好',
-          pictures: <String>[
-            'https://picsum.photos/250?image=9',
-          ],
-          user: User(
-            id: '1',
-            avatar: 'https://picsum.photos/250?image=9',
-            name: '张三',
-          ),
-          favorates: [
-            User(
-              id: '1',
-              avatar: 'https://picsum.photos/250?image=9',
-              name: '张三',
-            ),
-            User(
-              id: '2',
-              avatar: 'https://picsum.photos/250?image=21',
-              name: '李四',
-            ),
-            User(
-              id: '3',
-              avatar: 'https://picsum.photos/250?image=33',
-              name: '王五',
-            ),
-            User(
-              id: '4',
-              avatar: 'https://picsum.photos/250?image=37',
-              name: '赵六',
-            ),
-          ],
-          comments: [
-            Comment(
-              id: '1',
-              user: User(
-                id: '1',
-                avatar: 'https://picsum.photos/250?image=9',
-                name: '张三',
-              ),
-              content: '今天天气真好',
-            ),
-            Comment(
-              id: '2',
-              user: User(
-                id: '2',
-                avatar: 'https://picsum.photos/250?image=21',
-                name: '李四',
-              ),
-              content: '今天天气真好',
-            ),
-            Comment(
-              id: '3',
-              user: User(
-                id: '3',
-                avatar: 'https://picsum.photos/250?image=33',
-                name: '王五',
-              ),
-              content: '今天天气真好',
-            ),
-            Comment(
-              id: '4',
-              user: User(
-                id: '4',
-                avatar: 'https://picsum.photos/250?image=37',
-                name: '赵六',
-              ),
-              content: '今天天气真好',
-            ),
-          ]),
-      Moment(
-          id: '9',
-          content: '今天天气真好',
-          pictures: <String>[
-            'https://picsum.photos/250?image=41',
-            'https://picsum.photos/250?image=42',
-            'https://picsum.photos/250?image=43',
-            'https://picsum.photos/250?image=49',
-          ],
-          user: User(
-            id: '9',
-            avatar: 'https://picsum.photos/250?image=41',
-            name: '钱十一',
-          )),
-      Moment(
-          id: '10',
-          content: '今天天气真好',
-          pictures: <String>[
-            'https://picsum.photos/250?image=41',
-            'https://picsum.photos/250?image=42',
-            'https://picsum.photos/250?image=43',
-            'https://picsum.photos/250?image=49',
-          ],
-          user: User(
-            id: '9',
-            avatar: 'https://picsum.photos/250?image=41',
-            name: '钱十一',
-          ),
-          favorates: [
-            User(
-              id: '1',
-              avatar: 'https://picsum.photos/250?image=9',
-              name: '张三',
-            ),
-            User(
-              id: '2',
-              avatar: 'https://picsum.photos/250?image=21',
-              name: '李四',
-            ),
-            User(
-              id: '3',
-              avatar: 'https://picsum.photos/250?image=33',
-              name: '王五',
-            ),
-            User(
-              id: '4',
-              avatar: 'https://picsum.photos/250?image=37',
-              name: '赵六',
-            ),
-          ],
-          essay: Essay(
-            id: '1',
-            title: '今天天气真好',
-            description: '天气真好',
-            cover: 'https://picsum.photos/250?image=9',
-          )),
+          avatar: 'images/Luxe7.jpg',
+          name: 'Luxe7',
+        ),
+      )
+      // Moment(
+      //     id: '1',
+      //     content: '今天天气真好',
+      //     pictures: <String>[
+      //       'https://picsum.photos/250?image=9',
+      //     ],
+      //     user: User(
+      //       id: '1',
+      //       avatar: 'https://picsum.photos/250?image=9',
+      //       name: '张三',
+      //     ),
+      //     favorates: [
+      //       User(
+      //         id: '1',
+      //         avatar: 'https://picsum.photos/250?image=9',
+      //         name: '张三',
+      //       ),
+      //       User(
+      //         id: '2',
+      //         avatar: 'https://picsum.photos/250?image=21',
+      //         name: '李四',
+      //       ),
+      //       User(
+      //         id: '3',
+      //         avatar: 'https://picsum.photos/250?image=33',
+      //         name: '王五',
+      //       ),
+      //       User(
+      //         id: '4',
+      //         avatar: 'https://picsum.photos/250?image=37',
+      //         name: '赵六',
+      //       ),
+      //     ],
+      //     comments: [
+      //       Comment(
+      //         id: '1',
+      //         user: User(
+      //           id: '1',
+      //           avatar: 'https://picsum.photos/250?image=9',
+      //           name: '张三',
+      //         ),
+      //         content: '今天天气真好',
+      //       ),
+      //       Comment(
+      //         id: '2',
+      //         user: User(
+      //           id: '2',
+      //           avatar: 'https://picsum.photos/250?image=21',
+      //           name: '李四',
+      //         ),
+      //         content: '今天天气真好',
+      //       ),
+      //       Comment(
+      //         id: '3',
+      //         user: User(
+      //           id: '3',
+      //           avatar: 'https://picsum.photos/250?image=33',
+      //           name: '王五',
+      //         ),
+      //         content: '今天天气真好',
+      //       ),
+      //       Comment(
+      //         id: '4',
+      //         user: User(
+      //           id: '4',
+      //           avatar: 'https://picsum.photos/250?image=37',
+      //           name: '赵六',
+      //         ),
+      //         content: '今天天气真好',
+      //       ),
+      //     ]),
+      // Moment(
+      //     id: '9',
+      //     content: '今天天气真好',
+      //     pictures: <String>[
+      //       'https://picsum.photos/250?image=41',
+      //       'https://picsum.photos/250?image=42',
+      //       'https://picsum.photos/250?image=43',
+      //       'https://picsum.photos/250?image=49',
+      //     ],
+      //     user: User(
+      //       id: '9',
+      //       avatar: 'https://picsum.photos/250?image=41',
+      //       name: '钱十一',
+      //     )),
+      // Moment(
+      //     id: '10',
+      //     content: '今天天气真好',
+      //     pictures: <String>[
+      //       'https://picsum.photos/250?image=41',
+      //       'https://picsum.photos/250?image=42',
+      //       'https://picsum.photos/250?image=43',
+      //       'https://picsum.photos/250?image=49',
+      //     ],
+      //     user: User(
+      //       id: '9',
+      //       avatar: 'https://picsum.photos/250?image=41',
+      //       name: '钱十一',
+      //     ),
+      //     favorates: [
+      //       User(
+      //         id: '1',
+      //         avatar: 'https://picsum.photos/250?image=9',
+      //         name: '张三',
+      //       ),
+      //       User(
+      //         id: '2',
+      //         avatar: 'https://picsum.photos/250?image=21',
+      //         name: '李四',
+      //       ),
+      //       User(
+      //         id: '3',
+      //         avatar: 'https://picsum.photos/250?image=33',
+      //         name: '王五',
+      //       ),
+      //       User(
+      //         id: '4',
+      //         avatar: 'https://picsum.photos/250?image=37',
+      //         name: '赵六',
+      //       ),
+      //     ],
+      //     essay: Essay(
+      //       id: '1',
+      //       title: '今天天气真好',
+      //       description: '天气真好',
+      //       cover: 'https://picsum.photos/250?image=9',
+      //     )),
     ];
   }
 
